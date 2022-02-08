a8b9a1f4bb9ddf186166c4e8d8e1caf3c65c7ba8
Codiving <codivingmoon@gmail.com>
2022-02-08 20:55:01 +0900

    Grid 추가

---
+++ src/components/GridTemplateColumns/GridTemplateColumns.js
@@ -0,0 +1,5 @@
+const GridTemplateColumns = () => {
+  return <div></div>;
+};
+
+export default GridTemplateColumns;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Flex } from "./Flex";
 export { default as FlexBasis } from "./FlexBasis";
 export { default as FlexGrow } from "./FlexGrow";
 export { default as FlexShrink } from "./FlexShrink";
+export { default as Grid } from "./Grid";
